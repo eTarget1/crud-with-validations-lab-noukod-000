b1c2e0edfa38001f2e5c65ac65cadc4972e90069
clifford <clifford@noukod.com>
2019-01-31 21:59:15 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  # get 'songs/new'
-  # get 'songs/create'
-  # get 'songs/edit'
-  # get 'songs/update'
-  # get 'songs/destroy'
-  # get 'songs/index'
-  # get 'songs/show'
-   resources :songs
+  get 'songs/new'
+  get 'songs/create'
+  get 'songs/edit'
+  get 'songs/update'
+  get 'songs/destroy'
+  get 'songs/index'
+  get 'songs/show'
+  #  resources :songs
 end
