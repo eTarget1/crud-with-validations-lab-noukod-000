b4171d04213dc6e391889cedb1670186925a8aaa
clifford <clifford@noukod.com>
2019-01-31 21:53:45 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,23 @@
+class SongsController < ApplicationController
+  def new
+    @song = Song.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
